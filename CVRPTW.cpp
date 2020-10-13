85996e68f451f9ef8336213a88d2b4bb2a578ee8
Michal Dziecielski <dziecielski.m@gmail.com>
2020-10-13 08:59:08 -0700

    Odczytałem testowy plik

--- CVRPTW.cpp
+++ CVRPTW.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
     std::cout << "Hello GitHub!\n";
+
+    std::ifstream example_input;
+    example_input.open("Input/cvrptw1.txt");
+
+    std::string line;
+
+    while (std::getline(example_input, line)) {
+        std::cout << line << std::endl;
+    }
+
+    example_input.close();
+    
+    return 0;
 }
