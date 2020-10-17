f57ef93b3c49ab2274b8717dec8a4d4181d497a0
Michał Dzięcielski <Michal@MacBook-Pro-Micha.local>
2020-10-17 18:04:45 +0200

    Added some classes

--- CVRPTW.cpp
+++ CVRPTW.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+
+class Customer {
+    public:
+        Customer(int X, int Y, int DEMAND, int READY_TIME, int END_TIME, int SERVICE_TIME) {
+            this->x = X;
+            this->y = Y;
+            this->demand = DEMAND;
+            this->ready_time = READY_TIME;
+            this->end_time = END_TIME;
+            this->service_time = SERVICE_TIME;
+        };
+
+        int get_x() {
+            return this->x;
+        }
+
+        int get_y() {
+            return this->y;
+        }
+
+        int get_demand() {
+            return this->demand;
+        }
+
+        int get_ready_time() {
+            return this->ready_time;
+        }
+
+        int get_end_time() {
+            return this->end_time;
+        }
+
+        int get_service_time() {
+            return this->service_time;
+        }
+
+    private:
+        int x;
+        int y;
+        int demand;
+        int ready_time;
+        int end_time;    
+        int service_time;
+};
+
+class CVRPTW {
+    public:
+        CVRPTW(int VEHICLE_NUMBER, int VEHICLE_CAPACITY) {
+            this->vehicle_number = VEHICLE_NUMBER;
+            this->vehicle_capacity = VEHICLE_CAPACITY;
+        };
+
+        void add_customer(Customer customer) {
+            this->customers.push_back(customer);
+        }
+
+    private:
+        int vehicle_number;
+        int vehicle_capacity;
+        std::vector<Customer> customers;
+};
 
 int main()
 {
